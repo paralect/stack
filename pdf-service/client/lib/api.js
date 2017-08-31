9bbc8f2cc1fdd95e333805f8b861b15c04ebdc25
Artuom Kukharenko <a.kukahrenko@paralect.com>
2017-08-31 13:26:13 +0300

    [-]fix pipes in dev mode
    [*]change version

--- pdf-service/client/lib/api.js
+++ pdf-service/client/lib/api.js
@@ -1,5 +1,6 @@
 const fs = require('./promiseFs');
 const path = require('path');
+const { PassThrough } = require('stream');
 const fetchService = require('./fetchService');
 const logger = require('./logger');
 const Handlebars = require('handlebars');
@@ -69,11 +70,16 @@ const getPdfFromHtml = async ({
 
   const pdfStream = getPdf(html, wkhtmltopdfOptions, serverUrl);
 
+  const [devStream, resultStream] = [
+    pdfStream.pipe(new PassThrough()),
+    pdfStream.pipe(new PassThrough()),
+  ];
+
   if (mode === 'development') {
-    await writePdf(pdfPath, pdfStream);
+    await writePdf(pdfPath, devStream);
   }
 
-  return pdfStream;
+  return resultStream;
 };
 
 module.exports = { getPdfFromHtml, isProdHtmlExists };
